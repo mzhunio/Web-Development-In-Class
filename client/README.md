10a976f851392cbe76b2bce410a99b9cd61fae8f
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-02-23 15:19:09 -0500

    vue.js

--- client/README.md
+++ client/README.md
@@ -1,3 +1,4 @@
+// put something about your client
 # client
 
 This template should help get you started developing with Vue 3 in Vite.
--- client/src/App.vue
+++ client/src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { objectExpression } from '@babel/types';
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
@@ -8,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="New Paltz Rocks!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
