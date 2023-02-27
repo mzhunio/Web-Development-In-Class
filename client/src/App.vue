d8b0baf57372fd5ab8fc94140cc0aff0e493d48a
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-02-27 15:50:46 -0500

    add bulma

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,16 +2,27 @@
 import { RouterLink, RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue';
 
+
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <NavBar />
-  
-  <div class="container">
-    
-    <RouterView />
-  </div>
-  
+
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <!-- <HelloWorld msg="New Paltz!" /> -->
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
 </template>
 
 <style scoped>
