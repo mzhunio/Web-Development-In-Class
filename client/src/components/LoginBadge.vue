cd389fd82e947571ef8181909e2d397c31a7191d
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-03-02 14:38:51 -0500

    added Loginbadge.vue

---
+++ client/src/components/LoginBadge.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+<template>
+   <div class="navbar-item">
+              <a class="button is-primary">
+                <span class="icon">
+                  <i class="fas fa-user"></i>
+                </span>
+                <strong>Login</strong>
+              </a>
+            </div>
+
+            <div class="navbar-item">
+              <a class="button is-warming">
+                <span class="icon">
+                  <i class="fas fa-user-plus"></i>
+                </span>
+                <strong>Register</strong>
+              </a>
+            </div>
+</template>
+<style scoped>
+</style>
