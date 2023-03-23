378d178c31d8526b82ad513956e26e30008ba304
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-03-23 14:19:42 -0400

    working API, serving up static files, & deep linking

--- server/index.js
+++ server/index.js
@@ -7,14 +7,22 @@ const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 app
-  .use(express.json())
-  .use(express.static(path.join(__dirname, '../client/dist')))
- 
-app  
-  .get('/', (req, res) => {
-    res.send('Hello World! from Express')
-  })
-  .use('/products', products)
+    .use(express.json())
+    .use(express.static(path.join(__dirname, '../client/dist')))
+
+
+// Actions
+app
+    .get('/api/v1/', (req, res) => {
+        res.send('Hello World! From Express')
+    })
+    .use('/api/v1/products', products)
+
+// Catch all
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+})
+
 
 
 app.listen(port, () => 
--- server/models/products.js
+++ server/models/products.js
@@ -1,32 +1,43 @@
-const data = require('../data/products.json');
+const data = require("../data/products.json");
 
-function getProducts(id) {
-    return data.getProducts.find(product => product.id === id);
+function getProducts() {
+  return data.products;
 }
 
-function getProductById() {
-    
+function getProductById(id) {
+  return data.products.find((product) => product.id === id);
 }
 
 function addProduct(product) {
-    product.id = data.products.length + 1;
-    data.products.push(product);
+  product.id = data.products.length + 1;
+  data.products.push(product);
 }
 
 function updateProduct(product) {
-    const index = data.products.findIndex(p => p.id == product.id);
-    data.products(index) = index;
+  const index = data.products.findIndex((p) => p.id === product.id);
+  data.products[index] = product;
 }
 
 function deleteProduct(id) {
-    const index = data.products.findIndex(p => p.id == index);
-    data.products.splice(index, 1);
+  const index = data.products.findIndex((p) => p.id === id);
+  data.products.splice(index, 1);
 }
 
-module.reports = {
-    getProducts,
-    getProductById,
-    addProduct,
-    updateProduct,
-    deleteProduct,
+function searchProducts(searchTerm) {
+  return data.products.filter((product) => {
+    return (
+      product.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      product.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      product.brand.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  });
+}
+
+module.exports = {
+  getProducts,
+  getProductById,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+  searchProducts,
 };
