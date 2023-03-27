a6832fb654a5e7d01a4e7ab5892c4428c463ca3d
Michelle Zhunio <63206155+mzhunio@users.noreply.github.com>
2023-03-27 16:26:35 -0400

    session wrapper for api fetch method

--- server/index.js
+++ server/index.js
@@ -12,6 +12,12 @@ app
     .use(express.json())
     .use(express.static(path.join(__dirname, '../client/dist')))
 
+    .use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+        next()
+    })
 
 // Actions
 app
